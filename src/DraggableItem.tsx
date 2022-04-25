42a4725885baeaa678d4a7d8b3dd0d10209656d9
vingeray <vingeray@gmail.com>
2022-04-25 12:13:10 +0800

    refactor: 💡 clean code

--- src/DraggableItem.tsx
+++ src/DraggableItem.tsx
@@ -1,36 +1,12 @@
-import { css } from '@emotion/react'
 import clsx from 'clsx'
 import { HTMLAttributes } from 'react'
+import './DraggableItem.css'
 
 export interface DraggableItemProps extends HTMLAttributes<HTMLDivElement> {}
 
 export default function DraggableItem({ className, ...props }: DraggableItemProps) {
   return (
-    <div
-      css={css`
-        user-select: none;
-        position: absolute;
-        z-index: 1;
-        cursor: pointer;
-
-        &.muuri-item-positioning {
-          z-index: 2;
-        }
-
-        &.muuri-item-dragging,
-        &.muuri-item-releasing {
-          z-index: 3;
-        }
-
-        &.muuri-item-dragging {
-          cursor: move;
-        }
-
-        &.muuri-item-hidden {
-          z-index: 0;
-        }
-      `}
-    >
+    <div className="draggable-item">
       <div className={clsx(className, 'draggable-item-content')} {...props} />
     </div>
   )
