e9395aa852c7eadd17cd509f17508c1d212d492e
vingeray <vingeray@gmail.com>
2022-04-26 12:57:13 +0800

    refactor: 💡 adjust reexport

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import DraggableGrid from './DraggableGrid'
 
 export * from './config'
 export type { DraggableGridHandle, DraggableGridProps } from './DraggableGrid'
-export * from './DraggableItem'
+export { default as DraggableItem } from './DraggableItem'
+export type { DraggableItemProps } from './DraggableItem'
+
 export default DraggableGrid
