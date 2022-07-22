10b67561caf3a65f1eaf9a15c07805bee6bf3bc3
VingeRay <vingeray@gmail.com>
2022-07-22 16:16:39 +0800

    [DraggableItem]: add forward refs (#6)
    * feat: 🎸 add forward ref  for draggable item
    
    * refactor: 💡 adjust forward refs

--- src/DraggableItem.tsx
+++ src/DraggableItem.tsx
@@ -1,12 +1,17 @@
 import clsx from 'clsx'
-import { HTMLAttributes } from 'react'
+import { forwardRef, HTMLAttributes } from 'react'
 
 export interface DraggableItemProps extends HTMLAttributes<HTMLDivElement> {}
 
-export default function DraggableItem({ className, ...props }: DraggableItemProps) {
-  return (
-    <div className={clsx('ruuri-draggable-item', 'draggable-item')}>
-      <div className={clsx(className, 'draggable-item-content')} {...props} />
-    </div>
-  )
-}
+const DraggableItem = forwardRef<HTMLDivElement, DraggableItemProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div className={clsx('ruuri-draggable-item', 'draggable-item')}>
+        <div className={clsx(className, 'draggable-item-content')} {...props} ref={ref} />
+      </div>
+    )
+  },
+)
+DraggableItem.displayName = 'DraggableItem'
+
+export default DraggableItem
